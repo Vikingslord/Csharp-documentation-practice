0eca59bf4c40f555ed34dfaaf318f2abf8c8ac94
Ahsanul Haque <ahsanulhaque1998@outlook.com>
2022-07-26 14:04:25 +0600

    Data Casting

---
+++ DataCasting.cs
@@ -0,0 +1,29 @@
+﻿namespace CsTryout
+{
+    class DataCasting
+    {
+        static void Main()
+        {
+            //Converting strings to Number(Int)
+            int first = 5;
+            int second = 15;
+
+            string message = first.ToString() + second.ToString(); 
+            Console.WriteLine(message);
+
+            //Converting Int to String
+            string firstString = "5";
+            string secondString = "15";
+
+            int sum = int.Parse(firstString) + int.Parse(secondString);
+            Console.WriteLine(sum);
+
+            //convert a string to a number using the Convert class
+            string value1 = "5";
+            string value2 = "7";
+            int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+            Console.WriteLine(result);
+
+        }
+    }
+}
