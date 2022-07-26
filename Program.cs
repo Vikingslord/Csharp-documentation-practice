d43292c2796159d7c8e650110f65643b383933dd
Ahsanul Haque <ahsanulhaque1998@outlook.com>
2022-07-26 12:54:30 +0600

    Array
    File for Array Practice

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Array;
+
+class Array
+{
+    static void arrayPractice()
+    {
+        //Creating a New Array[] of string
+        string[] friends = new string[4];//Friends array with 4 elements
+
+        //Assigning values to the Friends array
+        friends[0] = "Xavier";
+        friends[1] = "Kobe";
+        friends[2] = "Jie Long";
+        friends[3] = "Reuben";
+
+        foreach (var friend in friends) //Looping all the elements out of the Array
+        {
+            Console.WriteLine(friend);
+        }
+
+        //Another way of Creating an Array (easier way)
+        int[] ages = { 23,21,19,20};
+
+        foreach (var age in ages)
+        {
+            Console.WriteLine(age);
+        }
+    }
+
+    static void Main(string[] arguments) //Main method where everything will run
+    {
+        arrayPractice(); //All the code from Array Practice
+    }
+}
