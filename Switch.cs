6969dbd8a3095cb33183982bd6a4b65702afc917
Ahsanul Haque <ahsanulhaque1998@outlook.com>
2022-07-26 13:29:26 +0600

    Switch Statement

---
+++ Switch.cs
@@ -0,0 +1,36 @@
+﻿
+namespace CsTryout
+{
+    class Switch
+    {
+        static void Main(string[] arguments)
+        {
+            int employeeLevel = 200;
+            string employeeName = "John Smith";
+
+            string title = "";
+
+            switch (employeeLevel)
+            {
+                case 100:
+                    title = "Junior Associate";
+                    break;
+                case 200:
+                    title = "Associate";
+                    break;
+                case 300:
+                    title = "Senior Associate";
+                    break;
+                case 400:
+                    title = "Managing Partner";
+                    break;
+                default:
+                    title = "Founder";
+                    break;
+            }
+
+            Console.WriteLine($"{employeeName}, {title}");
+
+        }
+    }
+}
